a30eb75f914286f9d795a319c3c7cf92efffaa9e
LucianoAparecidoBritoGuedes <lucianobrito.dev@gmail.com>
2020-05-05 07:32:35 -0300

    First commit
    Adding project in github

---
+++ resources/js/carousel.js
@@ -0,0 +1,7 @@
+$(function() {
+   $('.carousel').carousel({
+       interval:4000,               //Intervalo entre um slide e outro
+       //pause:"null",              //faz o carrossel não parar quando o mouse estiver em cima
+       wrap:true                    //Retorna à primeira imagem do carrossel
+   });
+});
