3bbbf52fcfbe816295076f35de05677b0bf4f423
Luciano Brito <lucianobrito.dev@gmailcom>
2020-05-22 14:48:25 -0300

    add redesign and upgrade in structure of website

--- resources/js/carousel.js
+++ resources/js/carousel.js
@@ -1,6 +1,6 @@
 $(function() {
    $('.carousel').carousel({
-       interval:4000,               //Intervalo entre um slide e outro
+       interval:5000,               //Intervalo entre um slide e outro
        //pause:"null",              //faz o carrossel não parar quando o mouse estiver em cima
        wrap:true                    //Retorna à primeira imagem do carrossel
    });
