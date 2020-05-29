9814810da5ec5041b0f6bccfea15ed315f9c0b6f
LucianoAparecidoBritoGuedes <lucianobrito.dev@gmail.com>
2020-05-29 14:54:00 -0300

    update footer, serman and index.html

--- resources/js/carousel.js
+++ resources/js/carousel.js
@@ -1,7 +1,7 @@
 $(function() {
    $('.carousel').carousel({
        interval:5000,               //Intervalo entre um slide e outro
-       //pause:"null",              //faz o carrossel não parar quando o mouse estiver em cima
+       pause:"null",              //faz o carrossel não parar quando o mouse estiver em cima
        wrap:true                    //Retorna à primeira imagem do carrossel
    });
 });
